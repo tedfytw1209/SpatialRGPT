18030fa2dd77d34976225e7e4c60da51a83d616c
teddy <teddytw1209@gmail.com>
2025-01-02 11:33:19 +0800

    Update eval_region_vqa.py

--- llava/eval/eval_region_vqa.py
+++ llava/eval/eval_region_vqa.py
@@ -212,8 +212,6 @@ if __name__ == "__main__":
     parser.add_argument("--annotation-file", type=str, default="")
     parser.add_argument("--answers-file", type=str, default="answer.jsonl")
     parser.add_argument("--conv-mode", type=str, default="llava_v1")
-    parser.add_argument("--num-chunks", type=int, default=1)
-    parser.add_argument("--chunk-idx", type=int, default=0)
     parser.add_argument("--temperature", type=float, default=0.2)
     parser.add_argument("--top_p", type=float, default=None)
     parser.add_argument("--num_beams", type=int, default=1)
